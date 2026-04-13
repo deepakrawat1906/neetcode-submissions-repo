da0e712d9c613a7b59d329e5b32616f68004a412
deepakrawat1906 <149591751+deepakrawat1906@users.noreply.github.com>
2026-04-13 15:50:08 +0530

    Add: longest-repeating-substring-with-replacement - submission-0

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        countMap = {}
+        res = 0
+
+        l = 0
+
+        for r in range(len(s)):
+            countMap[s[r]] = 1 + countMap.get(s[r],0)
+            
+            while (r-l) - max(countMap.values()) >=k:
+                countMap[s[l]]-=1
+                l+=1
+
+            res = max(res,r-l+1)
+        
+        return res
+        
